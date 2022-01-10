bcd45cbe12156b1aec1d4033186a298bed828d0e
Tianyao Chen <tillchen417@gmail.com>
2022-01-10 11:40:19 +0800

    Finish is_number_palindromic

--- epi_judge_python/is_number_palindromic.py
+++ epi_judge_python/is_number_palindromic.py
@@ -1,8 +1,23 @@
 from test_framework import generic_test
+import math
 
+def is_palindrome_number_0(x: int) -> bool:
+    return str(x) == str(x)[::-1]
 
 def is_palindrome_number(x: int) -> bool:
-    # TODO - you fill in here.
+    # Check if x is a palindrom.
+    if x <= 0:
+        return x == 0
+    number_of_digits = math.floor(math.log10(x)) + 1
+    msn_mask = 10 ** (number_of_digits - 1)
+    for i in range(number_of_digits // 2):
+        lsn = x % 10
+        msn = x // msn_mask
+        if lsn != msn:
+            return False
+        x %= msn_mask
+        x //= 10
+        msn_mask //= 100
     return True
 
 
--- problem_mapping.js
+++ problem_mapping.js
@@ -136,7 +136,7 @@ problem_mapping = {
                 "total": 20000
             },
             "Python: is_number_palindromic.py": {
-                "passed": 0,
+                "passed": 20000,
                 "total": 20000
             }
         },
