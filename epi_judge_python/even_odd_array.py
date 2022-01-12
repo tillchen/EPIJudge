31ee73649b37398c3f4bf792e41b2f55d684e18a
Tianyao Chen <tillchen417@gmail.com>
2022-01-12 11:46:28 +0800

    Finish even_odd_array

--- epi_judge_python/even_odd_array.py
+++ epi_judge_python/even_odd_array.py
@@ -7,9 +7,22 @@ from test_framework.test_failure import TestFailure
 from test_framework.test_utils import enable_executor_hook
 
 
-def even_odd(A: List[int]) -> None:
-    # TODO - you fill in here.
+def even_odd_0(A: List[int]) -> None:
+    # Reorder array so that the even entries appear first.
+    evens = [x for x in A if x % 2 == 0]
+    odds = [x for x in A if x % 2 == 1]
+    A[:] = evens + odds
     return
+
+def even_odd(A: List[int]) -> None:
+    next_even = 0
+    next_odd = len(A) - 1
+    while next_even < next_odd:
+        if A[next_even] % 2 == 0:
+            next_even += 1
+        else:
+            A[next_even], A[next_odd] = A[next_odd], A[next_even]
+            next_odd -= 1
 
 
 @enable_executor_hook
