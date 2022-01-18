52fcefee7cb0b62d0869658f737bcd6a2fe15444
Tianyao Chen <tillchen417@gmail.com>
2022-01-18 22:05:02 +0800

    Finish dutch_national_flag.py

--- epi_judge_python/dutch_national_flag.py
+++ epi_judge_python/dutch_national_flag.py
@@ -17,9 +17,34 @@ def dutch_flag_partition_1(pivot_index: int, A: List[int]) -> None:
     greater = [x for x in A if x > A[pivot_index]]
     A[:] = smaller + equal + greater
 
+def dutch_flag_partition_2(pivot_index: int, A: List[int]) -> None:
+    pivot = A[pivot_index]
+    smaller = 0
+    for i in range(len(A)):
+        if A[i] < pivot:
+            A[i], A[smaller] = A[smaller], A[i]
+            smaller += 1
+    bigger = len(A) - 1
+    for i in reversed(range(len(A))):
+        if A[i] < pivot:
+            break
+        elif A[i] > pivot:
+            A[i], A[bigger] = A[bigger], A[i]
+            bigger -= 1
+
 def dutch_flag_partition(pivot_index: int, A: List[int]) -> None:
-
-
+    pivot = A[pivot_index]
+    smaller, equal, larger = 0, 0, len(A) - 1
+    while equal <= larger:
+        if A[equal] < pivot:
+            A[equal], A[smaller] = A[smaller], A[equal]
+            smaller += 1
+            equal += 1
+        elif A[equal] == pivot:
+            equal += 1
+        else:
+            A[equal], A[larger] = A[larger], A[equal]
+            larger -= 1
 
 @enable_executor_hook
 def dutch_flag_partition_wrapper(executor, A, pivot_idx):
