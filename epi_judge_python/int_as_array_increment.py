c090cdcec55419ee44d70c94ca4c041f8657ea2f
Tianyao Chen <tillchen417@gmail.com>
2022-01-20 11:29:40 +0800

    Finish int_as_array_increment.py

--- epi_judge_python/int_as_array_increment.py
+++ epi_judge_python/int_as_array_increment.py
@@ -4,8 +4,17 @@ from test_framework import generic_test
 
 
 def plus_one(A: List[int]) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    # Add one to the number represented as a list.
+    A[-1] += 1
+    for i in reversed(range(1, len(A))):
+        if A[i] != 10:
+            break
+        A[i] = 0
+        A[i - 1] += 1
+    if A[0] == 10:
+        A[0] = 1
+        A.append(0)
+    return A
 
 
 if __name__ == '__main__':
