c97dcfc05f5d91cb8938f34766f273dc26ea1b41
Tianyao Chen <tillchen417@gmail.com>
2022-01-26 12:18:23 +0800

    Finish buy_and_sell_stock.py

--- epi_judge_python/buy_and_sell_stock.py
+++ epi_judge_python/buy_and_sell_stock.py
@@ -1,11 +1,15 @@
 from typing import List
-
+from math import inf
 from test_framework import generic_test
 
 
 def buy_and_sell_stock_once(prices: List[float]) -> float:
-    # TODO - you fill in here.
-    return 0.0
+    result = 0
+    current_min = inf
+    for price in prices:
+        current_min = min(current_min, price)
+        result = max(result, price - current_min)
+    return result
 
 
 if __name__ == '__main__':
