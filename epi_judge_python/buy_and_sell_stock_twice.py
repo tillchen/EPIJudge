c10eeac1f260086c70ac135b9fb9fb48a36b832b
Tianyao Chen <tillchen417@gmail.com>
2022-01-27 14:14:31 +0800

    Finish buy_and_sell_stock_twice.py

--- epi_judge_python/buy_and_sell_stock_twice.py
+++ epi_judge_python/buy_and_sell_stock_twice.py
@@ -13,14 +13,16 @@ def buy_and_sell_stock_twice(prices: List[float]) -> float:
         current_min = min(current_min, price)
         profit = max(profit, price - current_min)
         first_transaction_profits.append(profit)
+    # first_transaction_profits[-1] = 0 since we can't sell on the first day.
+    first_transaction_profits.append(0)
     total_profits = [0] * len(prices)
     current_max = -inf
     profit = 0
-    for i, price in reversed(list(enumerate(prices[1:], start=1))):
+    for i, price in reversed(list(enumerate(prices))):
         current_max = max(current_max, price)
         # Profit if buy on or after day i.
         profit = max(profit, current_max - price)
-        total_profits[i] = first_transaction_profits[i] + profit
+        total_profits[i] = first_transaction_profits[i - 1] + profit
     return max(total_profits)
 
 
