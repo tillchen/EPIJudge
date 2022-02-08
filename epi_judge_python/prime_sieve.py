94d8e825e11fd233ef36b3d8b208ba40bc7ea4d8
Tianyao Chen <tillchen417@gmail.com>
2022-02-08 11:37:21 +0800

    Finish prime_sieve.py

--- epi_judge_python/prime_sieve.py
+++ epi_judge_python/prime_sieve.py
@@ -1,13 +1,34 @@
 from typing import List
+from math import sqrt
 
 from test_framework import generic_test
 
 
 # Given n, return all primes up to and including n.
-def generate_primes(n: int) -> List[int]:
-    # TODO - you fill in here.
-    return []
+def generate_primes_brute_force(n: int) -> List[int]:
+    # O(n sqrt(n))
+    result = []
+    for i in range(2, n + 1):
+        if is_prime(i):
+            result.append(i)
+    return result
 
+def generate_primes(n: int) -> List[int]:
+    # O(n log log n)
+    result = []
+    is_prime_sieve = [False, False] + [True] * (n - 1)
+    for i in range(2, n + 1):
+        if is_prime_sieve[i]:
+            result.append(i)
+            for j in range(2 * i, n + 1, i):
+                is_prime_sieve[j] = False
+    return result
+
+def is_prime(n: int) -> bool:
+    for i in range(2, int(sqrt(n)) + 1):
+        if n % i == 0:
+            return False
+    return True
 
 if __name__ == '__main__':
     exit(
--- problem_mapping.js
+++ problem_mapping.js
@@ -306,7 +306,7 @@ problem_mapping = {
                 "total": 24
             },
             "Python: prime_sieve.py": {
-                "passed": 0,
+                "passed": 24,
                 "total": 24
             }
         },
