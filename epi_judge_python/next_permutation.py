057abad822719a97ef8ca2d15a39fa05c6855616
Tianyao Chen <tillchen417@gmail.com>
2022-02-18 11:51:00 +0800

    Finish next_permutation.py

--- epi_judge_python/next_permutation.py
+++ epi_judge_python/next_permutation.py
@@ -13,14 +13,16 @@ def next_permutation(perm: List[int]) -> List[int]:
             break
     if turning_point is None:
         return []
-    if turning_point == len(perm) - 1:
-        swap_index = turning_point - 1
-    else:
-        swap_target = min(x for x in perm[turning_point + 1:] if x > perm[turning_point])
-        swap_index = perm.index(swap_target, turning_point+1)
     result = perm[:]
-    result[turning_point], result[swap_index] = result[swap_index], result[turning_point]
-    result[turning_point + 1:] = sorted(result[turning_point + 1:])
+    if turning_point == len(perm) - 2:
+        result[turning_point], result[turning_point + 1] = result[turning_point + 1], result[turning_point]
+        return result
+    # Since the suffix is decreasing, the first (in the reverse order) element bigger than turning_point is our swap.
+    for i in reversed(range(turning_point + 1, len(perm))):
+        if result[i] > result[turning_point]:
+            result[turning_point], result[i] = result[i], result[turning_point]
+            break
+    result[turning_point + 1:] = reversed(result[turning_point + 1:])
     return result
 
 
