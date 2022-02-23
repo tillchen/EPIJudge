d98bef1745ba4a6fdf959ca638a29d425b45840d
Tianyao Chen <tillchen417@gmail.com>
2022-02-23 11:36:45 +0800

    Finish random_subset.py

--- epi_judge_python/random_subset.py
+++ epi_judge_python/random_subset.py
@@ -9,9 +9,18 @@ from test_framework.test_utils import enable_executor_hook
 
 import random
 
-def random_subset(n: int, k: int) -> List[int]:
+def random_subset_0(n: int, k: int) -> List[int]:
     return random.sample(range(n), k)
 
+def random_subset(n: int, k: int) -> List[int]:
+    changed_elements = {}
+    for i in range(k):
+        random_index = random.randrange(i, n)
+        random_index_mapped = changed_elements.get(random_index, random_index)
+        i_mapped = changed_elements.get(i, i)
+        changed_elements[random_index] = i_mapped
+        changed_elements[i] = random_index_mapped
+    return [changed_elements[i] for i in range(k)]
 
 @enable_executor_hook
 def random_subset_wrapper(executor, n, k):
