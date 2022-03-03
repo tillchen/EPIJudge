9dc26f5944d2675184f41dac73a7429ad9fc5095
Tianyao Chen <tillchen417@gmail.com>
2022-03-03 11:45:57 +0800

    Finish spiral_ordering.py

--- epi_judge_python/spiral_ordering.py
+++ epi_judge_python/spiral_ordering.py
@@ -21,6 +21,23 @@ def matrix_in_spiral_order(square_matrix: List[List[int]]) -> List[int]:
 
     return spiral_ordering
 
+def matrix_in_spiral_order(square_matrix: List[List[int]]) -> List[int]:
+    SHIFT = ((0, 1), (1, 0), (0, -1), (-1, 0))
+    direction = x = y = 0
+    spiral_ordering = []
+
+    for _ in range(len(square_matrix) ** 2):
+        spiral_ordering.append(square_matrix[x][y])
+        square_matrix[x][y] = 0
+        next_x, next_y = x + SHIFT[direction][0], y + SHIFT[direction][1]
+        if (next_x not in range(len(square_matrix)) or
+            next_y not in range(len(square_matrix)) or
+            square_matrix[next_x][next_y] == 0
+        ):
+            direction = (direction + 1) % 4
+            next_x, next_y = x + SHIFT[direction][0], y + SHIFT[direction][1]
+        x, y = next_x, next_y
+    return spiral_ordering
 
 if __name__ == '__main__':
     exit(
