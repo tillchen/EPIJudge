266844c7567217e99092fef4dc3cd9be15a8f2c0
Tianyao Chen <tillchen417@gmail.com>
2022-03-10 22:36:06 +0800

    Finish string_integer_interconversion.py

--- epi_judge_python/string_integer_interconversion.py
+++ epi_judge_python/string_integer_interconversion.py
@@ -1,15 +1,31 @@
 from test_framework import generic_test
 from test_framework.test_failure import TestFailure
 
+from collections import deque
 
 def int_to_string(x: int) -> str:
-    # TODO - you fill in here.
-    return '0'
+    is_negative = x < 0
+    x = abs(x)
+    result = deque()
+    while x:
+        result.appendleft(str(x % 10))
+        x //= 10
+    if is_negative:
+        result.appendleft('-')
+    return ''.join(result) if result else '0'
 
 
 def string_to_int(s: str) -> int:
-    # TODO - you fill in here.
-    return 0
+    is_negative = False
+    if s[0] == '-':
+        is_negative = True
+        s = s[1:]
+    elif s[0] == '+':
+        s = s[1:]
+    result = 0
+    for i, c in enumerate(s[::-1]):
+        result += int(c) * 10 ** i
+    return result if not is_negative else -result
 
 
 def wrapper(x, s):
