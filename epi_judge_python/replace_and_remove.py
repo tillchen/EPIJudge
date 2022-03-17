1ba68e8bb3d662fa5ef2e3b95cb2a790662a8cac
Tianyao Chen <tillchen417@gmail.com>
2022-03-17 21:32:36 +0800

    Finish replace_and_remove.py

--- epi_judge_python/replace_and_remove.py
+++ epi_judge_python/replace_and_remove.py
@@ -5,9 +5,29 @@ from test_framework import generic_test
 from test_framework.test_utils import enable_executor_hook
 
 
+# a -> dd, remove b
 def replace_and_remove(size: int, s: List[str]) -> int:
-    # TODO - you fill in here.
-    return 0
+    # First pass, remove 'b's and count 'a's.
+    write_index, a_count = 0, 0
+    for i, c in enumerate(s[:size]):
+        if c == 'a':
+            a_count += 1
+        if c != 'b':
+            s[write_index] = s[i]
+            write_index += 1
+    # Second pass, a -> dd from the back.
+    total_size = write_index + a_count
+    current_index = write_index - 1
+    write_index = total_size - 1
+    while current_index > -1:
+        if s[current_index] == 'a':
+            s[write_index - 1] = s[write_index] = 'd'
+            write_index -= 2
+        else:
+            s[write_index] = s[current_index]
+            write_index -= 1
+        current_index -= 1
+    return total_size
 
 
 @enable_executor_hook
