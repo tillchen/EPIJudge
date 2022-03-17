b3f0d57b54ec6bc970aee555bb0cf0c39d7c57e2
Tianyao Chen <tillchen417@gmail.com>
2022-03-17 22:09:39 +0800

    Finish is_string_palindromic_punctuation.py

--- epi_judge_python/is_string_palindromic_punctuation.py
+++ epi_judge_python/is_string_palindromic_punctuation.py
@@ -1,9 +1,10 @@
 from test_framework import generic_test
+from string import punctuation
 
 
 def is_palindrome(s: str) -> bool:
-    # TODO - you fill in here.
-    return True
+    s = s.translate(str.maketrans('', '', punctuation)).replace(' ', '')
+    return s == s[::-1]
 
 
 if __name__ == '__main__':
