6822613c5a9f3025a122e9f0832ae067db276161
Tianyao Chen <tillchen417@gmail.com>
2022-03-20 20:04:10 +0800

    Init reverse_words

--- epi_judge_python/reverse_words.py
+++ epi_judge_python/reverse_words.py
@@ -3,12 +3,20 @@ import functools
 from test_framework import generic_test
 from test_framework.test_utils import enable_executor_hook
 
+from typing import List
+
 
 # Assume s is a list of strings, each of which is of length 1, e.g.,
 # ['r', 'a', 'm', ' ', 'i', 's', ' ', 'c', 'o', 's', 't', 'l', 'y'].
-def reverse_words(s):
-    # TODO - you fill in here.
-    return
+def reverse_words(s: List[str]):
+    if not s:
+        return []
+    s[:] = ''.join(s).split(' ')[::-1]
+    result = []
+    for x in s:
+        result += list(x) + [' ']
+    del result[-1]
+    s[:] = result
 
 
 @enable_executor_hook
