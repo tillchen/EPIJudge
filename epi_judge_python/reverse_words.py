447c42cd930659eba5bdb6e874b7a8db7a733329
Tianyao Chen <tillchen417@gmail.com>
2022-03-20 20:19:48 +0800

    Finish reverse_words.py

--- epi_judge_python/reverse_words.py
+++ epi_judge_python/reverse_words.py
@@ -8,7 +8,7 @@ from typing import List
 
 # Assume s is a list of strings, each of which is of length 1, e.g.,
 # ['r', 'a', 'm', ' ', 'i', 's', ' ', 'c', 'o', 's', 't', 'l', 'y'].
-def reverse_words(s: List[str]):
+def reverse_words_0(s: List[str]):
     if not s:
         return []
     s[:] = ''.join(s).split(' ')[::-1]
@@ -17,6 +17,17 @@ def reverse_words(s: List[str]):
         result += list(x) + [' ']
     del result[-1]
     s[:] = result
+
+def reverse_words(s: List[str]):
+    s.reverse()
+    s.append(' ')
+    i, j = 0, 0
+    while i < len(s) and j < len(s):
+        if s[j] == ' ':
+            s[i:j] = s[i:j][::-1]
+            i = j + 1
+        j += 1
+    del s[-1]
 
 
 @enable_executor_hook
