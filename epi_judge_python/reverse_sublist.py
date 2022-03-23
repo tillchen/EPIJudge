91c805d38b9ecc7ceeb018c57edb380c9f227cb1
Tianyao Chen <tillchen417@gmail.com>
2022-03-23 11:47:15 +0800

    Init reverse_sublist.py

--- epi_judge_python/reverse_sublist.py
+++ epi_judge_python/reverse_sublist.py
@@ -1,3 +1,4 @@
+from multiprocessing import cpu_count
 from typing import Optional
 
 from list_node import ListNode
@@ -6,8 +7,26 @@ from test_framework import generic_test
 
 def reverse_sublist(L: ListNode, start: int,
                     finish: int) -> Optional[ListNode]:
-    # TODO - you fill in here.
-    return None
+    if not L or start == finish:
+        return L
+    connection_left, current = ListNode(), L
+    connection_left.next = L
+    for _ in range(start - 1):
+        connection_left = current
+        current = current.next
+    for _ in range(finish - start):
+        current = current.next
+        connection_right = current.next
+    previous, current = None, connection_left.next
+    while current is not connection_right:
+        next = current.next
+        current.next = previous
+        previous = current
+        current = next
+    if connection_left.next:
+        connection_left.next.next = connection_right
+    connection_left.next = previous
+    return L if start != 1 else previous
 
 
 if __name__ == '__main__':
