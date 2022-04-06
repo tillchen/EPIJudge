1d742a348f5f4000ba359827fde7b53fcca6150b
Tianyao Chen <tillchen417@gmail.com>
2022-04-06 10:45:26 +0800

    Finish int_as_list_add.py

--- epi_judge_python/int_as_list_add.py
+++ epi_judge_python/int_as_list_add.py
@@ -5,8 +5,19 @@ from test_framework import generic_test
 
 
 def add_two_numbers(L1: ListNode, L2: ListNode) -> Optional[ListNode]:
-    # TODO - you fill in here.
-    return None
+    head = current = ListNode()
+    is_carry_over = False
+    while L1 or L2 or is_carry_over:
+        current_value = (L1.data if L1 else 0) + (L2.data if L2 else 0)
+        if is_carry_over:
+            current_value += 1
+        is_carry_over = True if current_value > 9 else False
+        current_value %= 10
+        current.next = ListNode(current_value)
+        current = current.next
+        L1 = L1.next if L1 else None
+        L2 = L2.next if L2 else None
+    return head.next
 
 
 if __name__ == '__main__':
