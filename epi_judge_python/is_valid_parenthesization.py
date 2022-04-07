3474bbc971e85b9a91768e5d1f4d2912c9454092
Tianyao Chen <tillchen417@gmail.com>
2022-04-07 20:37:06 +0800

    Finish is_valid_parenthesization.py

--- epi_judge_python/is_valid_parenthesization.py
+++ epi_judge_python/is_valid_parenthesization.py
@@ -1,9 +1,21 @@
 from test_framework import generic_test
+from collections import deque
 
 
 def is_well_formed(s: str) -> bool:
-    # TODO - you fill in here.
-    return True
+    pairs = {
+        '(': ')',
+        '[': ']',
+        '{': '}'
+    }
+    stack = deque()
+    for c in s:
+        if c in pairs:
+            stack.append(c)
+        else:
+            if not stack or pairs[stack.pop()] != c:
+                return False
+    return not stack
 
 
 if __name__ == '__main__':
