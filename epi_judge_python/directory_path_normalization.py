c3cc15f5ed50f9c83ab29093a688b22e9341c8a1
Tianyao Chen <tillchen417@gmail.com>
2022-04-07 21:58:35 +0800

    Finish directory_path_normalization.py

--- epi_judge_python/directory_path_normalization.py
+++ epi_judge_python/directory_path_normalization.py
@@ -1,9 +1,21 @@
 from test_framework import generic_test
+from collections import deque
 
 
 def shortest_equivalent_path(path: str) -> str:
-    # TODO - you fill in here.
-    return ''
+    if not path:
+        return path
+    stack = deque()
+    for x in path.split('/'):
+        if x == '..':
+            if stack and stack[-1] != '..':
+                stack.pop()
+            else:
+                stack.append('..')
+        elif x and x != '.':
+            stack.append(x)
+    result = '/'.join(stack)
+    return '/' + result if path[0] == '/' else result
 
 
 if __name__ == '__main__':
