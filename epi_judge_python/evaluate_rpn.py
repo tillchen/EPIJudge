c7a49b0b024fabb00bd232fcdb377dd188640ad8
Tianyao Chen <tillchen417@gmail.com>
2022-04-07 11:22:34 +0800

    Finish evaluate_rpn.py

--- epi_judge_python/evaluate_rpn.py
+++ epi_judge_python/evaluate_rpn.py
@@ -1,9 +1,21 @@
 from test_framework import generic_test
+from collections import deque
 
 
 def evaluate(expression: str) -> int:
-    # TODO - you fill in here.
-    return 0
+    stack = deque()
+    symbols = {
+        '+': lambda y, x: x + y,
+        '-': lambda y, x: x - y,
+        '*': lambda y, x: x * y,
+        '/': lambda y, x: x // y
+    }
+    for c in expression.split(','):
+        if c not in symbols:
+            stack.append(int(c))
+        else:
+            stack.append(symbols[c](stack.pop(), stack.pop()))
+    return stack[0]
 
 
 if __name__ == '__main__':
