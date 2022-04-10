527d231454545de2f642a751030e5af14abc3ba2
Tianyao Chen <tillchen417@gmail.com>
2022-04-10 22:12:06 +0800

    Finish lowest_common_ancestor.py

--- epi_judge_python/lowest_common_ancestor.py
+++ epi_judge_python/lowest_common_ancestor.py
@@ -7,11 +7,24 @@ from test_framework.binary_tree_utils import must_find_node, strip_parent_link
 from test_framework.test_failure import TestFailure
 from test_framework.test_utils import enable_executor_hook
 
+from collections import namedtuple
 
 def lca(tree: BinaryTreeNode, node0: BinaryTreeNode,
         node1: BinaryTreeNode) -> Optional[BinaryTreeNode]:
-    # TODO - you fill in here.
-    return None
+    Status = namedtuple('Status', 'num_of_target_nodes ancestor')
+
+    def helper(tree: BinaryTreeNode, node0: BinaryTreeNode, node1: BinaryTreeNode) -> Status:
+        if not tree:
+            return Status(0, None)
+        left_result = helper(tree.left, node0, node1)
+        if left_result.num_of_target_nodes == 2:
+            return left_result
+        right_result = helper(tree.right, node0, node1)
+        if right_result.num_of_target_nodes == 2:
+            return right_result
+        num_of_target_nodes = left_result.num_of_target_nodes + right_result.num_of_target_nodes + int(tree is node0) + int(tree is node1)
+        return Status(num_of_target_nodes, tree if num_of_target_nodes == 2 else None)
+    return helper(tree, node0, node1).ancestor
 
 
 @enable_executor_hook
