e8915eae5f52b1d2a8cd460eda797edc6c48e25c
Tianyao Chen <tillchen417@gmail.com>
2022-04-10 21:54:22 +0800

    Finish is_tree_symmetric.py

--- epi_judge_python/is_tree_symmetric.py
+++ epi_judge_python/is_tree_symmetric.py
@@ -3,8 +3,16 @@ from test_framework import generic_test
 
 
 def is_symmetric(tree: BinaryTreeNode) -> bool:
-    # TODO - you fill in here.
-    return True
+
+    def are_two_trees_mirrored(tree_0: BinaryTreeNode, tree_1: BinaryTreeNode) -> bool:
+        if tree_0 and tree_1:
+            return tree_0.data == tree_1.data and are_two_trees_mirrored(tree_0.left, tree_1.right) and are_two_trees_mirrored(tree_0.right, tree_1.left)
+        elif not tree_0 and not tree_1:
+            return True
+        else:
+            return False
+
+    return not tree or are_two_trees_mirrored(tree.left, tree.right)
 
 
 if __name__ == '__main__':
