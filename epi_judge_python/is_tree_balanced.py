5bd1624940687796fc0aae8e64e88e8bdd704092
Tianyao Chen <tillchen417@gmail.com>
2022-04-10 21:44:18 +0800

    Finish is_tree_balanced.py

--- epi_judge_python/is_tree_balanced.py
+++ epi_judge_python/is_tree_balanced.py
@@ -1,10 +1,26 @@
 from binary_tree_node import BinaryTreeNode
 from test_framework import generic_test
 
+from collections import namedtuple
+
 
 def is_balanced_binary_tree(tree: BinaryTreeNode) -> bool:
-    # TODO - you fill in here.
-    return True
+    BalancedStatusWithHeight = namedtuple('BalancedStatusWithHeight', 'is_balanced height')
+
+    def check_balanced(tree: BinaryTreeNode) -> BalancedStatusWithHeight:
+        if not tree:
+            return BalancedStatusWithHeight(True, -1)
+        left_result = check_balanced(tree.left)
+        if not left_result.is_balanced:
+            return BalancedStatusWithHeight(False, 0)
+        right_result = check_balanced(tree.right)
+        if not right_result.is_balanced:
+            return BalancedStatusWithHeight(False, 0)
+        is_balanced = abs(left_result.height - right_result.height) < 2
+        height = max(left_result.height, right_result.height) + 1
+        return BalancedStatusWithHeight(is_balanced, height)
+
+    return check_balanced(tree).is_balanced
 
 
 if __name__ == '__main__':
