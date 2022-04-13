96f4d8f60c39a45c26f3bccf439369b746708a64
Tianyao Chen <tillchen417@gmail.com>
2022-04-13 16:42:21 +0800

    Finish tree_inorder.py

--- epi_judge_python/tree_inorder.py
+++ epi_judge_python/tree_inorder.py
@@ -3,11 +3,32 @@ from typing import List
 from binary_tree_node import BinaryTreeNode
 from test_framework import generic_test
 
+from collections import deque
+
+def inorder_traversal_0(tree: BinaryTreeNode) -> List[int]:
+    result = []
+    def helper(tree: BinaryTreeNode, path: List[int]) -> None:
+        if not tree:
+            return
+        helper(tree.left, path)
+        path.append(tree.data)
+        helper(tree.right, path)
+
+    helper(tree, result)
+    return result
 
 def inorder_traversal(tree: BinaryTreeNode) -> List[int]:
-    # TODO - you fill in here.
-    return []
-
+    stack = deque()
+    result = []
+    while stack or tree:
+        if tree:
+            stack.append(tree)
+            tree = tree.left
+        else:
+            tree = stack.pop()
+            result.append(tree.data)
+            tree = tree.right
+    return result
 
 if __name__ == '__main__':
     exit(
