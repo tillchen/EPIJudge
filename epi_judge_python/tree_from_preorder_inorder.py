5ef54966e00b51c48e1f0c0ba71c2872e149b995
Tianyao Chen <tillchen417@gmail.com>
2022-04-21 22:37:08 +0800

    Finish tree_with_parent_inorder.py

--- epi_judge_python/tree_from_preorder_inorder.py
+++ epi_judge_python/tree_from_preorder_inorder.py
@@ -1,21 +1,48 @@
+import enum
 from typing import List
 
 from binary_tree_node import BinaryTreeNode
 from test_framework import generic_test
 
 
-def binary_tree_from_preorder_inorder(preorder: List[int],
+def binary_tree_from_preorder_inorder_0(preorder: List[int],
                                       inorder: List[int]) -> BinaryTreeNode:
     if not inorder:
         return
     root_val = preorder.pop(0)
     root = BinaryTreeNode(root_val)
     root_index = inorder.index(root_val)
-    root.left = binary_tree_from_preorder_inorder(preorder, inorder[:root_index])
-    root.right = binary_tree_from_preorder_inorder(preorder, inorder[root_index+1:])
+    root.left = binary_tree_from_preorder_inorder_0(preorder, inorder[:root_index])
+    root.right = binary_tree_from_preorder_inorder_0(preorder, inorder[root_index+1:])
     return root
 
+def binary_tree_from_preorder_inorder_1(preorder: List[int], inorder: List[int]) -> BinaryTreeNode:
+    inorder_value_to_index = {v: k for k, v in enumerate(inorder)}
+    def helper(preorder: List[int], inorder: List[int]) -> BinaryTreeNode:
+        if not inorder:
+            return
+        root_val = preorder.pop(0)
+        root = BinaryTreeNode(root_val)
+        root_index = inorder_value_to_index[root_val]
+        root.left = binary_tree_from_preorder_inorder_1(preorder, inorder[:root_index])
+        root.right = binary_tree_from_preorder_inorder_1(preorder, inorder[root_index+1:])
+        return root
+    return helper(preorder, inorder)
 
+def binary_tree_from_preorder_inorder(preorder: List[int], inorder: List[int]) -> BinaryTreeNode:
+    inorder_value_to_index = {v: k for k, v in enumerate(inorder)}
+    def helper(preorder_start: int, preorder_end: int, inorder_start: int, inorder_end) -> BinaryTreeNode:
+        if preorder_end <= preorder_start or inorder_end <= inorder_start:
+            return
+        root_val = preorder[preorder_start]
+        root_index = inorder_value_to_index[root_val]
+        left_subtree_size = root_index - inorder_start
+        return BinaryTreeNode(
+            root_val,
+            helper(preorder_start + 1, preorder_start + 1 + left_subtree_size, inorder_start, root_index),
+            helper(preorder_start + 1 + left_subtree_size, preorder_end, root_index + 1, inorder_end)
+        )
+    return helper(0, len(preorder), 0, len(inorder))
 
 if __name__ == '__main__':
     exit(
