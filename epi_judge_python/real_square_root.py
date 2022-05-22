0064273348d3dcae339f4d454fab5a5512d6e060
Tianyao Chen <tillchen417@gmail.com>
2022-05-22 15:50:05 +0800

    Finish real_square_root.py

--- epi_judge_python/real_square_root.py
+++ epi_judge_python/real_square_root.py
@@ -1,9 +1,17 @@
 from test_framework import generic_test
+import math
 
 
 def square_root(x: float) -> float:
-    # TODO - you fill in here.
-    return 0.0
+    low, high = (1.0, x) if x > 1.0 else (x, 1.0)
+    while not math.isclose(low, high):
+        mid = (low + high) / 2
+        mid_squared = mid * mid
+        if mid_squared > x:
+            high = mid
+        else:
+            low = mid
+    return low
 
 
 if __name__ == '__main__':
