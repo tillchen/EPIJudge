0f1c1df9e1b23696a0caebb418a2ad31e9ab31df
Tianyao Chen <tillchen417@gmail.com>
2022-05-22 15:33:34 +0800

    Finish int_sqaure_root.py

--- epi_judge_python/int_square_root.py
+++ epi_judge_python/int_square_root.py
@@ -2,8 +2,17 @@ from test_framework import generic_test
 
 
 def square_root(k: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    low, high = 0, k
+    while low <= high:
+        mid = low + (high - low) // 2
+        mid_squared = mid * mid
+        if mid_squared < k:
+            low = mid + 1
+        elif mid_squared > k:
+            high = mid - 1
+        else:
+            return mid
+    return low - 1
 
 
 if __name__ == '__main__':
