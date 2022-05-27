528864756fcb61392729c9d55a89c39f8ed0f73c
Tianyao Chen <tillchen417@gmail.com>
2022-05-27 22:35:42 +0800

    Finish anagrams.py

--- epi_judge_python/anagrams.py
+++ epi_judge_python/anagrams.py
@@ -2,10 +2,14 @@ from typing import List
 
 from test_framework import generic_test, test_utils
 
+from collections import defaultdict
+
 
 def find_anagrams(dictionary: List[str]) -> List[List[str]]:
-    # TODO - you fill in here.
-    return []
+    anagrams = defaultdict(list)
+    for x in dictionary:
+        anagrams[''.join(sorted(x))].append(x)
+    return [x for x in anagrams.values() if len(x) > 1]
 
 
 if __name__ == '__main__':
