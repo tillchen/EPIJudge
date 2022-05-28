744389faa4c78de849a89b4d8095310da2cb7e7b
Tianyao Chen <tillchen417@gmail.com>
2022-05-28 20:50:52 +0800

    Finish is_string_permutable_to_palindrome.py

--- epi_judge_python/is_string_permutable_to_palindrome.py
+++ epi_judge_python/is_string_permutable_to_palindrome.py
@@ -3,7 +3,7 @@ from test_framework import generic_test
 from collections import Counter
 
 def can_form_palindrome(s: str) -> bool:
-    return len([x for x in Counter(s).values() if x % 2 == 1]) in (0, 1)
+    return len([x for x in Counter(s).values() if x % 2 == 1]) < 2
 
 
 if __name__ == '__main__':
