4736056a1d2acea34fd58f75a588dd94166c4a56
Tianyao Chen <tillchen417@gmail.com>
2022-05-30 14:33:38 +0800

    Finish lru_cache.py

--- epi_judge_python/lru_cache.py
+++ epi_judge_python/lru_cache.py
@@ -1,23 +1,30 @@
 from test_framework import generic_test
 from test_framework.test_failure import TestFailure
 
+from collections import OrderedDict
 
 class LruCache:
     def __init__(self, capacity: int) -> None:
-        # TODO - you fill in here.
+        self.capacity = capacity
+        self.isbns_to_prices = OrderedDict()
         return
 
     def lookup(self, isbn: int) -> int:
-        # TODO - you fill in here.
-        return 0
+        if isbn not in self.isbns_to_prices:
+            return -1
+        self.isbns_to_prices.move_to_end(isbn)
+        return self.isbns_to_prices[isbn]
 
     def insert(self, isbn: int, price: int) -> None:
-        # TODO - you fill in here.
-        return
+        if isbn in self.isbns_to_prices:
+            self.isbns_to_prices.move_to_end(isbn)
+        else:
+            if len(self.isbns_to_prices) == self.capacity:
+                self.isbns_to_prices.popitem(last=False)
+            self.isbns_to_prices[isbn] = price
 
     def erase(self, isbn: int) -> bool:
-        # TODO - you fill in here.
-        return True
+        return self.isbns_to_prices.pop(isbn, None) is not None
 
 
 def lru_cache_tester(commands):
