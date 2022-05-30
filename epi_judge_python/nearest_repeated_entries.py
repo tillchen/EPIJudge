690943283ed1f67bb4931ef8864608c47ca942a8
Tianyao Chen <tillchen417@gmail.com>
2022-05-30 22:03:57 +0800

    Finish nearest_repeated_entries.py

--- epi_judge_python/nearest_repeated_entries.py
+++ epi_judge_python/nearest_repeated_entries.py
@@ -4,8 +4,13 @@ from test_framework import generic_test
 
 
 def find_nearest_repetition(paragraph: List[str]) -> int:
-    # TODO - you fill in here.
-    return 0
+    words_to_indices = {}
+    result = float('inf')
+    for i, word in enumerate(paragraph):
+        if word in words_to_indices:
+            result = min(result, i - words_to_indices[word])
+        words_to_indices[word] = i
+    return result if result != float('inf') else -1
 
 
 if __name__ == '__main__':
