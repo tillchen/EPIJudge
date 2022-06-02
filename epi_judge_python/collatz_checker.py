fea1e2586b5d5a62d1bc3c6c75f340b99b3ada34
Tianyao Chen <tillchen417@gmail.com>
2022-06-02 21:12:35 +0800

    Finish collatz_checker.py

--- epi_judge_python/collatz_checker.py
+++ epi_judge_python/collatz_checker.py
@@ -2,8 +2,23 @@ from test_framework import generic_test
 
 
 def test_collatz_conjecture(n: int) -> bool:
-    # TODO - you fill in here.
-    return False
+    verified_numbers = set()
+    for i in range(3, n + 1):
+        sequence = set()
+        current = i
+        while current >= i:
+            if current in sequence:
+                # Infinite loop.
+                return False
+            sequence.add(current)
+            if current % 2 == 1:
+                if current in verified_numbers:
+                    break
+                verified_numbers.add(current)
+                current = current * 3 + 1
+            else:
+                current //= 2
+    return True
 
 
 if __name__ == '__main__':
