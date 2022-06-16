9a14e50327e097f04063fdcbeeb55a6693aebb5d
Tianyao Chen <tillchen417@gmail.com>
2022-06-16 23:19:43 +0800

    Finish bst_from_preorder.py

--- epi_judge_python/bst_from_preorder.py
+++ epi_judge_python/bst_from_preorder.py
@@ -7,18 +7,31 @@ from test_framework import generic_test
 def rebuild_bst_from_preorder_0(preorder_sequence: List[int]) -> Optional[BstNode]:
     if not preorder_sequence:
         return
-    pivot = next((i for i, a in enumerate(preorder_sequence) if a > preorder_sequence[0]), len(preorder_sequence))
-    return BstNode(preorder_sequence[0], rebuild_bst_from_preorder(preorder_sequence[1:pivot]), rebuild_bst_from_preorder(preorder_sequence[pivot:]))
-
-def rebuild_bst_from_preorder(preorder_sequence: List[int]) -> Optional[BstNode]:
-    if not preorder_sequence:
-        return
     pivot = len(preorder_sequence)
     for i, a in enumerate(preorder_sequence):
         if a > preorder_sequence[0]:
             pivot = i
             break
-    return BstNode(preorder_sequence[0], rebuild_bst_from_preorder(preorder_sequence[1:pivot]), rebuild_bst_from_preorder(preorder_sequence[pivot:]))
+    return BstNode(preorder_sequence[0], rebuild_bst_from_preorder_0(preorder_sequence[1:pivot]), rebuild_bst_from_preorder_0(preorder_sequence[pivot:]))
+
+def rebuild_bst_from_preorder_1(preorder_sequence: List[int]) -> Optional[BstNode]:
+    if not preorder_sequence:
+        return
+    pivot = next((i for i, a in enumerate(preorder_sequence) if a > preorder_sequence[0]), len(preorder_sequence))
+    return BstNode(preorder_sequence[0], rebuild_bst_from_preorder_1(preorder_sequence[1:pivot]), rebuild_bst_from_preorder_1(preorder_sequence[pivot:]))
+
+def rebuild_bst_from_preorder(preorder_sequence: List[int]) -> Optional[BstNode]:
+    root_index = 0
+    def helper(lower_bound: int, upper_bound: int) -> Optional[BstNode]:
+        nonlocal root_index
+        if root_index == len(preorder_sequence):
+            return None
+        root = preorder_sequence[root_index]
+        if not lower_bound <= root <= upper_bound:
+            return None
+        root_index += 1
+        return BstNode(root, helper(lower_bound, root), helper(root, upper_bound))
+    return helper(float('-inf'), float('inf'))
 
 if __name__ == '__main__':
     exit(
