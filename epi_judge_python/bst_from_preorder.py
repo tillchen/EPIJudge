045e01d83c8ebc3ee20baa60350e9fa0957c9066
Tianyao Chen <tillchen417@gmail.com>
2022-06-16 22:57:18 +0800

    Finish bst_from_preorder.py

--- epi_judge_python/bst_from_preorder.py
+++ epi_judge_python/bst_from_preorder.py
@@ -4,12 +4,21 @@ from bst_node import BstNode
 from test_framework import generic_test
 
 
-def rebuild_bst_from_preorder(preorder_sequence: List[int]) -> Optional[BstNode]:
+def rebuild_bst_from_preorder_0(preorder_sequence: List[int]) -> Optional[BstNode]:
     if not preorder_sequence:
         return
     pivot = next((i for i, a in enumerate(preorder_sequence) if a > preorder_sequence[0]), len(preorder_sequence))
     return BstNode(preorder_sequence[0], rebuild_bst_from_preorder(preorder_sequence[1:pivot]), rebuild_bst_from_preorder(preorder_sequence[pivot:]))
 
+def rebuild_bst_from_preorder(preorder_sequence: List[int]) -> Optional[BstNode]:
+    if not preorder_sequence:
+        return
+    pivot = len(preorder_sequence)
+    for i, a in enumerate(preorder_sequence):
+        if a > preorder_sequence[0]:
+            pivot = i
+            break
+    return BstNode(preorder_sequence[0], rebuild_bst_from_preorder(preorder_sequence[1:pivot]), rebuild_bst_from_preorder(preorder_sequence[pivot:]))
 
 if __name__ == '__main__':
     exit(
