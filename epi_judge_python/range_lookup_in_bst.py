31436417702426cb8bad1faa5c364f9737f22baf
Tianyao Chen <tillchen417@gmail.com>
2022-06-20 11:49:12 +0800

    Finish range_lookup_in_bst.py

--- epi_judge_python/range_lookup_in_bst.py
+++ epi_judge_python/range_lookup_in_bst.py
@@ -7,7 +7,7 @@ from test_framework import generic_test
 Interval = collections.namedtuple('Interval', ('left', 'right'))
 
 
-def range_lookup_in_bst(tree: BstNode, interval: Interval) -> List[int]:
+def range_lookup_in_bst_0(tree: BstNode, interval: Interval) -> List[int]:
     result = []
     def inorder_traversal(tree: BstNode):
         if not tree:
@@ -17,6 +17,22 @@ def range_lookup_in_bst(tree: BstNode, interval: Interval) -> List[int]:
             result.append(tree.data)
         inorder_traversal(tree.right)
     inorder_traversal(tree)
+    return result
+
+def range_lookup_in_bst(tree: BstNode, interval: Interval) -> List[int]:
+    result = []
+    def helper(tree: BstNode):
+        if not tree:
+            return
+        if interval.left <= tree.data <= interval.right:
+            helper(tree.left)
+            result.append(tree.data)
+            helper(tree.right)
+        elif tree.data < interval.left:
+            helper(tree.right)
+        else:
+            helper(tree.left)
+    helper(tree)
     return result
 
 
