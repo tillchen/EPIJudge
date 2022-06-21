1409a6e1fbab8be23ea7f3dfd5c46548dbd2c9b2
Tianyao Chen <tillchen417@gmail.com>
2022-06-21 15:59:45 +0800

    Finish hanoi.py

--- epi_judge_python/hanoi.py
+++ epi_judge_python/hanoi.py
@@ -9,8 +9,17 @@ NUM_PEGS = 3
 
 
 def compute_tower_hanoi(num_rings: int) -> List[List[int]]:
-    # TODO - you fill in here.
-    return []
+    result = []
+    pegs = [list(reversed(range(1, num_rings + 1)))] + [[] for _ in range(1, NUM_PEGS)]
+    def helper(num_rings_to_move: int, from_peg: int, to_peg: int, use_peg: int):
+        if num_rings_to_move == 0:
+            return
+        helper(num_rings_to_move - 1, from_peg, use_peg, to_peg)
+        pegs[to_peg].append(pegs[from_peg].pop())
+        result.append([from_peg, to_peg])
+        helper(num_rings_to_move - 1, use_peg, to_peg, from_peg)
+    helper(num_rings, 0, 1, 2)
+    return result
 
 
 @enable_executor_hook
