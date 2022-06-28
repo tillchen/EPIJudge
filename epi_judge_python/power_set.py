5eb76372f8233bdfd3a8afae0a410047e293c29b
Tianyao Chen <tillchen417@gmail.com>
2022-06-28 22:35:01 +0800

    Finish power_set.py

--- epi_judge_python/power_set.py
+++ epi_judge_python/power_set.py
@@ -5,9 +5,20 @@ from test_framework import generic_test, test_utils
 import itertools
 from itertools import combinations
 
-def generate_power_set(input_set: List[int]) -> List[List[int]]:
+def generate_power_set_0(input_set: List[int]) -> List[List[int]]:
     return list(itertools.chain.from_iterable(map(list, combinations(input_set, x)) for x in range(len(input_set) + 1)))
 
+def generate_power_set(input_set: List[int]) -> List[List[int]]:
+    result = []
+    def helper(to_be_selected: int, selected_so_far: List[int]):
+        if to_be_selected == len(input_set):
+            result.append(list(selected_so_far))
+            return
+        helper(to_be_selected + 1, selected_so_far)
+        helper(to_be_selected + 1, selected_so_far + [input_set[to_be_selected]])
+
+    helper(0, [])
+    return result
 
 if __name__ == '__main__':
     exit(
