974c7f2b3f83b8059bf3fd2952921cf070973536
Tianyao Chen <tillchen417@gmail.com>
2022-07-03 14:26:19 +0800

    Finish enumerate_balanced_parentheses.py

--- epi_judge_python/enumerate_balanced_parentheses.py
+++ epi_judge_python/enumerate_balanced_parentheses.py
@@ -4,8 +4,25 @@ from test_framework import generic_test, test_utils
 
 
 def generate_balanced_parentheses(num_pairs: int) -> List[str]:
-    # TODO - you fill in here.
-    return []
+    result = []
+    def directed_generate_balanced_parentheses(num_left_parens_needed,
+                                               num_right_parens_needed,
+                                               valid_prefix):
+        if not num_right_parens_needed:
+            result.append(valid_prefix)
+            return
+        if num_left_parens_needed > 0:  # Able to insert '('.
+            directed_generate_balanced_parentheses(num_left_parens_needed - 1,
+                                                   num_right_parens_needed,
+                                                   valid_prefix + '(')
+        if num_left_parens_needed < num_right_parens_needed:
+            # Able to insert ')'.
+            directed_generate_balanced_parentheses(num_left_parens_needed,
+                                                   num_right_parens_needed - 1,
+                                                   valid_prefix + ')')
+
+    directed_generate_balanced_parentheses(num_pairs, num_pairs, valid_prefix='')
+    return result
 
 
 if __name__ == '__main__':
