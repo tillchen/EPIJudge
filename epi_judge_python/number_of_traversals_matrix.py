7a46358a103ff789acf300b11d04c4b8da4b62b7
Tianyao Chen <tillchen417@gmail.com>
2022-07-04 22:02:36 +0800

    Finish number_of_traversal_matrix.py

--- epi_judge_python/number_of_traversals_matrix.py
+++ epi_judge_python/number_of_traversals_matrix.py
@@ -2,11 +2,21 @@ from test_framework import generic_test
 from functools import lru_cache
 
 @lru_cache
-def number_of_ways(n: int, m: int) -> int:
+def number_of_ways_0(n: int, m: int) -> int:
     if n == 1 or m == 1:
         return 1
-    return number_of_ways(n - 1, m) + number_of_ways(n, m - 1)
+    return number_of_ways_0(n - 1, m) + number_of_ways_0(n, m - 1)
 
+def number_of_ways(n: int, m: int) -> int:
+    memo = [[-1] * m for _ in range(n)]
+    def helper(n: int, m: int) -> int:
+        if memo[n][m] == -1:
+            if n == 0 or m == 0:
+                memo[n][m] = 1
+            else:
+                memo[n][m] = helper(n - 1, m) + helper(n, m - 1)
+        return memo[n][m]
+    return helper(n - 1, m - 1)
 
 if __name__ == '__main__':
     exit(
