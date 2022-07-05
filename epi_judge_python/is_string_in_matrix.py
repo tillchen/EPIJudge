15529f9ce8f83266b94ee15b90aa5dc130f2bbf5
Tianyao Chen <tillchen417@gmail.com>
2022-07-05 09:52:55 +0800

    Finish is_string_in_matrix.py

--- epi_judge_python/is_string_in_matrix.py
+++ epi_judge_python/is_string_in_matrix.py
@@ -3,10 +3,21 @@ from typing import List
 from test_framework import generic_test
 
 
-def is_pattern_contained_in_grid(grid: List[List[int]],
-                                 pattern: List[int]) -> bool:
-    # TODO - you fill in here.
-    return True
+def is_pattern_contained_in_grid(grid: List[List[int]], pattern: List[int]) -> bool:
+    memo = set()
+    def helper(x: int, y: int, offset: int) -> bool:
+        if offset == len(pattern):
+            return True
+        if (
+            0 <= x < len(grid) and 0 <= y < len(grid[0])
+            and grid[x][y] == pattern[offset]
+            and (x, y, offset) not in memo
+            and any(helper(x + a, y + b, offset + 1) for a, b in ((1, 0), (-1, 0), (0, 1), (0, -1)))
+        ):
+            return True
+        memo.add((x, y, offset))
+        return False
+    return any(helper(i, j, 0) for i in range(len(grid)) for j in range(len(grid[0])))
 
 
 if __name__ == '__main__':
--- problem_mapping.js
+++ problem_mapping.js
@@ -2198,7 +2198,7 @@ problem_mapping = {
                 "total": 5906
             },
             "Python: binomial_coefficients.py": {
-                "passed": 5906,
+                "passed": 0,
                 "total": 5906
             }
         },
@@ -2212,7 +2212,7 @@ problem_mapping = {
                 "total": 186
             },
             "Python: is_string_in_matrix.py": {
-                "passed": 0,
+                "passed": 186,
                 "total": 186
             }
         },
