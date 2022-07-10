46b8fc2b5ba6d01af583ff3c32b7bc175fa34d85
Tianyao Chen <tillchen417@gmail.com>
2022-07-10 15:14:03 +0800

    Finish longest_nondecreasing_subsequence.py

--- epi_judge_python/longest_nondecreasing_subsequence.py
+++ epi_judge_python/longest_nondecreasing_subsequence.py
@@ -6,7 +6,7 @@ from test_framework import generic_test
 def longest_nondecreasing_subsequence_length(A: List[int]) -> int:
     memo = [1] * len(A)
     for i in range(1, len(A)):
-        memo[i] = max(memo[i], 1 + max([memo[j] for j in range(i) if A[j] <= A[i]], default=0))
+        memo[i] = 1 + max([memo[j] for j in range(i) if A[j] <= A[i]], default=0)
     return max(memo)
 
 if __name__ == '__main__':
