bdbc6153e608e045d52f594d3f8e909ccfb789ba
Tianyao Chen <tillchen417@gmail.com>
2022-07-10 21:56:40 +0800

    Finish minimum_waiting_time.py

--- epi_judge_python/minimum_waiting_time.py
+++ epi_judge_python/minimum_waiting_time.py
@@ -1,11 +1,11 @@
 from typing import List
 
 from test_framework import generic_test
+from itertools import accumulate
 
 
 def minimum_total_waiting_time(service_times: List[int]) -> int:
-    # TODO - you fill in here.
-    return 0
+    return sum(list(accumulate(sorted(service_times)))[:-1])
 
 
 if __name__ == '__main__':
