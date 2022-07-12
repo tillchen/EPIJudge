5af3d75f57cf6c5512778e9af9c8435d38a589e8
Tianyao Chen <tillchen417@gmail.com>
2022-07-12 16:16:34 +0800

    Finish majority_element.py

--- epi_judge_python/majority_element.py
+++ epi_judge_python/majority_element.py
@@ -3,8 +3,22 @@ from typing import Iterator
 from test_framework import generic_test
 from collections import Counter
 
-def majority_search(stream: Iterator[str]) -> str:
+def majority_search_0(stream: Iterator[str]) -> str:
     return Counter(stream).most_common(1)[0][0]
+
+def majority_search(stream: Iterator[str]) -> str:
+    result = ''
+    count = 0
+    for s in stream:
+        if s == result:
+            count += 1
+        else:
+            if count == 0:
+                result = s
+                count += 1
+            else:
+                count -= 1
+    return result
 
 
 def majority_search_wrapper(stream):
