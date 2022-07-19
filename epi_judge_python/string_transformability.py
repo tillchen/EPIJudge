7fc7c8f4ce31500248adaeba5b8e62e2b381b02e
Tianyao Chen <tillchen417@gmail.com>
2022-07-19 22:28:22 +0800

    Finish string_tranformability.py

--- epi_judge_python/string_transformability.py
+++ epi_judge_python/string_transformability.py
@@ -2,10 +2,25 @@ from typing import Set
 
 from test_framework import generic_test
 
+from collections import namedtuple
+from collections import deque
+from string import ascii_lowercase
 
 def transform_string(D: Set[str], s: str, t: str) -> int:
-    # TODO - you fill in here.
-    return 0
+    StringWithDistance = namedtuple('StringWithDistance', ('candidate', 'distance'))
+    queue = deque([StringWithDistance(s, 0)])
+    D.remove(s)
+    while queue:
+        current = queue.popleft()
+        if current.candidate == t:
+            return current.distance
+        for i in range(len(s)):
+            for c in ascii_lowercase:
+                candidate = current.candidate[:i] + c + current.candidate[i+1:]
+                if candidate in D:
+                    queue.append(StringWithDistance(candidate, current.distance + 1))
+                    D.remove(candidate)
+    return -1
 
 
 if __name__ == '__main__':
