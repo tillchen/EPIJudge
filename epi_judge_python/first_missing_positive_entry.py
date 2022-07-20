8619bd436675cacd58d0593fa1db00ec237de040
Tianyao Chen <tillchen417@gmail.com>
2022-07-20 17:23:58 +0800

    Init first_missing_positive_entry.py

--- epi_judge_python/first_missing_positive_entry.py
+++ epi_judge_python/first_missing_positive_entry.py
@@ -3,7 +3,7 @@ from typing import List
 from test_framework import generic_test
 
 
-def find_first_missing_positive(A: List[int]) -> int:
+def find_first_missing_positive_0(A: List[int]) -> int:
     A = [x for x in sorted(set(A)) if x > 0]
     result = 1
     for x in A:
@@ -12,6 +12,12 @@ def find_first_missing_positive(A: List[int]) -> int:
         result += 1
     return result
 
+def find_first_missing_positive(A: List[int]) -> int:
+    set_of_A = set(A)
+    result = 1
+    while result in set_of_A:
+        result += 1
+    return result
 
 if __name__ == '__main__':
     exit(
