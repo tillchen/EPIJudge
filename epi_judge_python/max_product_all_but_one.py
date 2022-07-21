92d1f50122a6928a4dae96d101ea807b1f57bde3
Tianyao Chen <tillchen417@gmail.com>
2022-07-21 15:16:53 +0800

    Finish max_product_all_but_one.py

--- epi_judge_python/max_product_all_but_one.py
+++ epi_judge_python/max_product_all_but_one.py
@@ -4,12 +4,29 @@ from test_framework import generic_test
 from operator import mul
 from functools import reduce
 
-def find_biggest_n_minus_one_product(A: List[int]) -> int:
+def find_biggest_n_minus_one_product_0(A: List[int]) -> int:
     result = 0
     for i in range(len(A)):
         result = max(result, reduce(mul, A[:i] + A[i+1:], 1))
     return result
 
+def find_biggest_n_minus_one_product(A: List[int]) -> int:
+    products = [1] * len(A)
+    products_in_reverse = [1] * len(A)
+    current = 1
+    for i, x in enumerate(A):
+        current *= x
+        products[i] = current
+    current = 1
+    for i in range(len(A) - 1, -1, -1):
+        current *= A[i]
+        products_in_reverse[i] = current
+    result = 0
+    for i in range(len(A)):
+        prefix = products[i - 1] if i - 1 >= 0 else 1
+        suffix = products_in_reverse[i + 1] if i + 1 < len(A) else 1
+        result = max(result, prefix * suffix)
+    return result
 
 if __name__ == '__main__':
     exit(
