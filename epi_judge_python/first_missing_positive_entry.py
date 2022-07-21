c72d275c892c4dd356e4eaf332e7c10fa5ecb93a
Tianyao Chen <tillchen417@gmail.com>
2022-07-21 10:19:44 +0800

    Finish first_missing_positive_entry.py

--- epi_judge_python/first_missing_positive_entry.py
+++ epi_judge_python/first_missing_positive_entry.py
@@ -12,12 +12,21 @@ def find_first_missing_positive_0(A: List[int]) -> int:
         result += 1
     return result
 
-def find_first_missing_positive(A: List[int]) -> int:
+def find_first_missing_positive_1(A: List[int]) -> int:
     set_of_A = set(A)
     result = 1
     while result in set_of_A:
         result += 1
     return result
+
+def find_first_missing_positive(A: List[int]) -> int:
+    for i in range(len(A)):
+        while 1 <= A[i] <= len(A) and A[i] != A[A[i] - 1]:
+            A[A[i] - 1], A[i] = A[i], A[A[i] - 1]
+    for i, x in enumerate(A):
+        if x != i + 1:
+            return i + 1
+    return len(A) + 1
 
 if __name__ == '__main__':
     exit(
