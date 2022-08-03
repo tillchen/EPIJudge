cda6eb790f3d008e27987f16de72cf1f86e77c48
Tianyao Chen <tillchen417@gmail.com>
2022-08-03 22:01:40 +0800

    Finish search_frequent_items.py

--- epi_judge_python/search_frequent_items.py
+++ epi_judge_python/search_frequent_items.py
@@ -2,14 +2,33 @@ from typing import Iterator, List
 
 from test_framework import generic_test, test_utils
 from collections import defaultdict
-
+from itertools import tee
+from collections import Counter
 
 # Finds the candidates which may occur > n / k times.
-def search_frequent_items(k: int, stream: Iterator[str]) -> List[str]:
+def search_frequent_items_0(k: int, stream: Iterator[str]) -> List[str]:
     x_to_frequency = defaultdict(int)
     for x in stream:
         x_to_frequency[x] += 1
     return [x for x, frequency in x_to_frequency.items() if frequency > sum(x_to_frequency.values()) / k]
+
+def search_frequent_items(k: int, stream: Iterator[str]) -> List[str]:
+    stream, stream_copy = tee(stream)
+    x_to_frequency = Counter()
+    n = 0
+    for x in stream:
+        n += 1
+        x_to_frequency[x] += 1
+        if len(x_to_frequency) == k:
+            for key in x_to_frequency:
+                x_to_frequency[key] -= 1
+            x_to_frequency = +x_to_frequency
+    for x in x_to_frequency:
+        x_to_frequency[x] = 0
+    for x in stream_copy:
+        if x in x_to_frequency:
+            x_to_frequency[x] += 1
+    return [x for x, frequency in x_to_frequency.items() if frequency > n / k]
 
 
 def search_frequent_items_wrapper(k, stream):
